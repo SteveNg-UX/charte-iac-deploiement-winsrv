9692aca540d97d6b4ddd4c21b71dba876352b1ad
SteveNg-UX <stevenguyen.2000@yahoo.fr>
2026-04-22 21:43:08 +0000

    Update 2 files
    - /modules/AD-Certificate.psm1
    - /data/AD-Certificate/donnee.yaml

--- modules/AD-Certificate.psm1
+++ modules/AD-Certificate.psm1
@@ -1,33 +1,71 @@
 $DONNEE = $(Charger-DonneYAML -CHEMIN_FICHIER_YAML "..\data\AD-Certificate\donnee.yaml").DONNEE
 $MDP_FICHIER_CERT = (Read-Host "Mot de passe utilisateur" -AsSecureString)
 
+if ((-not (Get-Command -Name "Get-ADDomain" -ErrorAction SilentlyContinue)) -and ($DONNEE.SRV_ADDS_ADDR -ne '')) {
+    Set-DnsClientServerAddress -ServerAddresses $DONNEE.SRV_ADDS_ADDR -InterfaceAlias "Ethernet"
+    Add-Computer -DomainName $DONNEE.CACommonName
+}
+
 if ((Get-WindowsFeature -Name "AD-Certificate").InstallState -ne 'Installed'){
     Install-WindowsFeature -Name "AD-Certificate" -IncludeManagementTools -IncludeAllSubFeature
 }
 
 Import-Module -Name "ADCSDeployment"
 
-if (-not (Get-Command -Name "Get-ADDomain" -ErrorAction SilentlyContinue)) {
-    Set-DnsClientServerAddress -ServerAddresses $DONNEE.SRV_ADDS_ADDR -InterfaceAlias "Ethernet"
-    Add-Computer -DomainName $DONNEE.CACommonName
+Switch -Exact ($DONNEE.CAType){
+    'StandaloneRootCa' {
+        $params = @{
+            CAType = $DONNEE.CAType
+            CryptoProviderName = $DONNEE.CryptoProviderName
+            HashAlgorithmName = $DONNEE.HashAlgorithmName
+            KeyLength = $DONNEE.KeyLength
+            AllowAdministratorInteraction = $DONNEE.AllowAdministratorInteraction
+            ValidityPeriod = $DONNEE.ValidityPeriod
+            ValidityPeriodUnits = $DONNEE.ValidityPeriodUnits
+            CADistinguishedNameSuffix = $DONNEE.CADistinguishedNameSuffix
+            DatabaseDirectory = $DONNEE.DatabaseDirectory
+            LogDirectory = $DONNEE.LogDirectory
+        }
+        Install-AdcsCertificationAuthority @params
+    }
+    'StandaloneSubordinateCA' {
+        $params = @{
+            CAType = $DONNEE.CAType
+            CryptoProviderName = $DONNEE.CryptoProviderName
+            HashAlgorithmName = $DONNEE.HashAlgorithmName
+            KeyLength = $DONNEE.KeyLength
+            AllowAdministratorInteraction = $DONNEE.AllowAdministratorInteraction
+            CADistinguishedNameSuffix = $DONNEE.CADistinguishedNameSuffix
+            DatabaseDirectory = $DONNEE.DatabaseDirectory
+            LogDirectory = $DONNEE.LogDirectory
+            ParentCA = $DONNEE.ParentCA
+            OutputCertRequestFile = $DONNEE.OutputCertRequestFile
+        }
+        Install-AdcsCertificationAuthority @params
+    }
+    'EnterpriseRootCa' {
+    }
+    'EnterpriseSubordinateCa' {
+        $params = @{
+            AllowAdministratorInteraction = $DONNEE.AllowAdministratorInteraction
+            ValidityPeriod = $DONNEE.ValidityPeriod
+            ValidityPeriodUnits = $DONNEE.ValidityPeriodUnits
+            CACommonName = $DONNEE.CACommonName
+            CADistinguishedNameSuffix = $DONNEE.CADistinguishedNameSuffix
+            CAType = $DONNEE.CAType
+            CryptoProviderName = $DONNEE.CryptoProviderName
+            DatabaseDirectory = $DONNEE.DatabaseDirectory
+            HashAlgorithmName = $DONNEE.HashAlgorithmName
+            IgnoreUnicode = $DONNEE.IgnoreUnicode
+            KeyLength = $DONNEE.KeyLength
+            LogDirectory = $DONNEE.LogDirectory
+            OutputCertRequestFile = $DONNEE.OutputCertRequestFile
+            OverwriteExistingCAinDS = $DONNEE.OverwriteExistingCAinDS
+            OverwriteExistingKey = $DONNEE.OverwriteExistingKey
+            ParentCA = $DONNEE.ParentCA
+            OverwriteExistingDatabase = $DONNEE.OverwriteExistingDatabase
+            CertFilePassword = $MDP_FICHIER_CERT
+        }
+        Install-AdcsCertificationAuthority @params
+    }
 }
-
-Install-AdcsCertificationAuthority `
-    -AllowAdministratorInteraction:$DONNEE.AllowAdministratorInteraction `
-    -ValidityPeriod $DONNEE.ValidityPeriod `
-    -ValidityPeriodUnits $DONNEE.ValidityPeriodUnits `
-    -CACommonName $DONNEE.CACommonName `
-    -CADistinguishedNameSuffix $DONNEE.CADistinguishedNameSuffix `
-    -CAType $DONNEE.CAType `
-    -CryptoProviderName $DONNEE.CryptoProviderName `
-    -DatabaseDirectory $DONNEE.DatabaseDirectory `
-    -HashAlgorithmName $DONNEE.HashAlgorithmName `
-    -IgnoreUnicode:$DONNEE.IgnoreUnicode `
-    -KeyLength $DONNEE.KeyLength `
-    -LogDirectory $DONNEE.LogDirectory `
-    -OutputCertRequestFile $DONNEE.OutputCertRequestFile `
-    -OverwriteExistingCAinDS:$DONNEE.OverwriteExistingCAinDS `
-    -OverwriteExistingKey:$DONNEE.OverwriteExistingKey `
-    -ParentCA $DONNEE.ParentCA `
-    -OverwriteExistingDatabase:$DONNEE.OverwriteExistingDatabase
-    -CertFilePassword $MDP_FICHIER_CERT
